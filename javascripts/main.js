9759f7ab6d46489c95ee1ee2559ef7c1382f3de4
Kristin Caron <krissycaron@gmail.com>
2017-04-27 19:19:01 -0500

    got the ajax call working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log("HI");
+$(document).ready(function(){
+
+	const apiKey = "";
+
+	console.log("getting here? ");
+
+	$("body").on("click", "#buttonPress", (e)=> {
+		console.log("button clicked");
+		loadWeatherData().then((result)=>{
+			console.log(result);
+		});
+	});
+
+
+	const loadWeatherData = (city)=> {
+		// console.log(city);
+		return new Promise ((resolve, reject) => {
+			$.ajax(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${apiKey}`)
+			.done((data)=> resolve(data))
+			.fail((error)=>reject("error", error));
+		});
+	};
+ 	// loadWeatherData();
+
+});
